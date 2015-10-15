48e4b8db1d1c9c8d5a5e3ffa7d5903d233c8752c
Dimitri Walters <me@dimi.io>
2015-10-15 12:59:04 -0400

    Implement stack

--- graph.js
+++ graph.js
@@ -1,1 +1,17 @@
-console.log('hello, world');
+function Stack() {
+    var arr = [];
+    return {
+        push: function(val) {
+            arr.push(val);
+        },
+        pop: function() {
+            return arr.pop();
+        },
+        top: function() {
+            return arr[arr.length - 1];
+        },
+        isEmpty: function() {
+            return arr.length === 0;
+        }
+    }
+}
