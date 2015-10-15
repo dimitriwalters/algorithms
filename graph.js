3b90defabefe02e7b9b6a341066af05eeb457048
Dimitri Walters <me@dimi.io>
2015-10-15 17:16:09 -0400

    Implement breadth first search

--- graph.js
+++ graph.js
@@ -43,7 +43,55 @@ function DFS(source, dest, graph) {
     return false;
 }
 
-// http://www.algolist.net/img/graphs/DFS/DFS-example-1.png
+function Queue() {
+    var arr = [];
+    return {
+        enqueue: function(val) {
+            arr.push(val);
+        },
+        dequeue: function() {
+            return arr.shift();
+        },
+        front: function() {
+            return arr[0];
+        },
+        isEmpty: function() {
+            return arr.length === 0;
+        }
+    }
+}
+
+function BFS(G, s) {
+    var UNDISCOVERED = "undiscovered";
+    var DISCOVERED = "discovered";
+    var vertices = [];
+    var neighbours = [];
+    var current;
+    var queue = new Queue();
+
+    for (var i=0; i<G.length; i++) {
+        vertices.push(UNDISCOVERED);
+    }
+
+    queue.enqueue(s);
+    vertices[s] = DISCOVERED;
+
+    while (!queue.isEmpty()) {
+        current = queue.dequeue();
+        neighbours = G[current];
+        for (var i=0; i<neighbours.length; i++) {
+            if (vertices[neighbours[i]] === UNDISCOVERED) {
+                queue.enqueue(neighbours[i]);
+                vertices[neighbours[i]] = DISCOVERED;
+            }
+        }
+    }
+
+    return vertices;
+}
+
 var graph = [[4], [4,5], [5], [1,2,5], [2,3,4], []];
+var graph2 = [[1, 2], [2], [], []];
 
 console.log(DFS(1, 3, graph));
+console.log(BFS(graph2, 0));
