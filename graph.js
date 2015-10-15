9335d13a2da2a2789e405c3d07f248b1fed3b23f
Dimitri Walters <me@dimi.io>
2015-10-15 17:32:49 -0400

    Refactor dfs to resemble bfs

--- graph.js
+++ graph.js
@@ -16,31 +16,33 @@ function Stack() {
     }
 }
 
-function DFS(source, dest, graph) {
-    var visited = [];
-    var stack = new Stack();
-    var current;
+function DFS(G, s) {
+    var UNDISCOVERED = "undiscovered";
+    var DISCOVERED = "discovered";
+    var vertices = [];
     var neighbours;
+    var current;
+    var stack = new Stack();
 
-    stack.push(source);
+    for (var i=0; i<G.length; i++) {
+        vertices[i] = UNDISCOVERED;
+    }
+
+    stack.push(s);
+    vertices[s] = DISCOVERED;
 
     while (!stack.isEmpty()) {
         current = stack.pop();
-        // console.log('current: ' + current);
-        visited.push(current);
-        neighbours = graph[current - 1];
-
+        neighbours = G[current];
         for (var i=neighbours.length-1; i>=0; i--) {
-            // console.log('neighbour: ' + neighbours[i]);
-            if (neighbours[i] === dest) {
-                return true;
-            } else if (visited.indexOf(neighbours[i]) === -1) {
+            if (vertices[neighbours[i]] === UNDISCOVERED) {
                 stack.push(neighbours[i]);
+                vertices[neighbours[i]] = DISCOVERED;
             }
         }
     }
 
-    return false;
+    return vertices;
 }
 
 function Queue() {
@@ -90,8 +92,18 @@ function BFS(G, s) {
     return vertices;
 }
 
-var graph = [[4], [4,5], [5], [1,2,5], [2,3,4], []];
-var graph2 = [[1, 2], [2], [], []];
+function arraysEqual(a1,a2) {
+    return JSON.stringify(a1) == JSON.stringify(a2);
+}
 
-console.log(DFS(1, 3, graph));
-console.log(BFS(graph2, 0));
+// http://pages.cpsc.ucalgary.ca/~jacobs/Courses/cpsc331/F08/tutorials/Figures/directed_graph_example1.gif
+var graph = [[1, 4], [2], [0,1], [2], [5], [4]];
+
+var resultsOfDFS = DFS(graph, 0);
+var resultsOfBFS = BFS(graph, 0);
+
+if (arraysEqual(resultsOfDFS, resultsOfBFS)) {
+    console.log(resultsOfDFS);
+} else {
+    console.log("error in implementations");
+}
